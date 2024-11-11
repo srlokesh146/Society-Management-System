ee5a11fb5b38680215a32fe0d5c3d200e674c53c
Lokesh shinde <lokesh.shinde@swiftrut.com>
2024-11-11 13:03:19 +0530

    visitor log

--- Backend/src/routes/visitor.route.js
+++ Backend/src/routes/visitor.route.js
@@ -4,7 +4,7 @@ const router=require("express").Router()
 //add visitor
 router.post("/addvisitor",auth,IsSecurity,VisitorController.CreateVisitor)
 //get visitor
-router.get("/viewvisitor",auth,IsSecurity,VisitorController.GetAllVisitor)
+router.get("/viewvisitor",auth,VisitorController.GetAllVisitor)
 
 //filter data
 router.get("/filter",auth,IsSecurity,VisitorController.FilterVisitor)
