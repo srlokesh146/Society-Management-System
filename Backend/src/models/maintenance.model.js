dc2f85d88046bf3732e4fb8b88dfc39b9da3552d
Lokesh shinde <lokesh.shinde@swiftrut.com>
2024-11-19 20:50:18 +0530

    backend changes

--- Backend/src/models/maintenance.model.js
+++ Backend/src/models/maintenance.model.js
@@ -1,26 +1,52 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, default: mongoose } = require("mongoose");
 
-const maintenanceschema= new Schema({
-    maintenanceAmount:{
-        type:Number,
-        required:true
+const maintenanceschema = new Schema(
+  {
+    maintenanceAmount: {
+      type: Number,
+      required: true,
     },
-    penaltyAmount:{
-        type:Number,
-        required:true
+    penaltyAmount: {
+      type: Number,
+      required: true,
     },
-    dueDate:{
-        type:Date,
-        required:true
+    dueDate: {
+      type: Date,
+      required: true,
     },
-    penaltyDay:{
-        type:String,
-        required:true
+    penaltyDay: {
+      type: String,
+      required: true,
     },
+    residentList: [
+      {
+        resident: {
+          type: mongoose.SchemaTypes.ObjectId,
+          ref: "residentType",
+          required: true,
+        },
+        residentType: {
+          type: String,
+          required:true,
+          enum: ["Owner", "Tenante"],
+        },
+        paymentStatus: {
+          type: String,
+          enum: ["pending", "online", "cash", "done"],
+          default: "pending",
+        },
+        penalty: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
     // maintenanceapplyer: {
     //     type: mongoose.Schema.Types.ObjectId,
     //     ref: "User"
     // },
-},{timestamps:true})
-const Maintenance = model("Maintenance",maintenanceschema)
-module.exports=Maintenance
+  },
+  { timestamps: true }
+);
+const Maintenance = model("Maintenance", maintenanceschema);
+module.exports = Maintenance;
