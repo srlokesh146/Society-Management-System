fe868dae3e304027e2a927948e794d2c32a2804a
srharsh138 <harsh.kotak@swiftrut.com>
2024-12-07 04:57:29 +0530

    somechanges

--- Backend/src/routes/CommunityQuestion.route.js
+++ Backend/src/routes/CommunityQuestion.route.js
@@ -4,4 +4,5 @@ const router = require("express").Router();
 router.post("/createquestion", auth, QuestionController.createQuestion);
 router.post("/questions/:questionId/answers", QuestionController.addAnswer);
 router.get("/questions", QuestionController.GetQuestion);
+router.post("/:questionId/vote", auth,QuestionController.voteOnQuestion);
 module.exports = router;
